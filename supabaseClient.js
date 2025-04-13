9dd7e99aae9c6d9b1760b5114357eb3d89f4652b
l-eeoonn <cigaleon@gmail.com>
2025-04-13 23:23:40 +0200

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://usaftzvgptmalquawjql.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
